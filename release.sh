2d85f0398851458daec19c3d210eb1feb0e5031a
Patrick Lee Scott <pat@patscott.io>
2025-03-31 17:42:14 -0500

    chore: temp release script til action is made

---
+++ release.sh
@@ -0,0 +1,12 @@
+VERSION=`vnext`
+mkdir .tmp
+cd .tmp
+git clone git@github.com:harmony-labs/vnext.git
+cd vnext
+set-cargo-version Cargo.toml $VERSION
+git add -A
+git commit -m "chore(version): $VERSION"
+git tag $VERSION
+git push --tags
+cd ../../
+rm -rf .tmp/vnext
